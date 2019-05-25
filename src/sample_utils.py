5462ed99cc0be5b3386b11e9afddbd3226fa125a
rob cheung <kousun12@gmail.com>
2019-05-25 09:32:57 -0400

    --wip-- [skip ci]

--- src/sample_utils.py
+++ src/sample_utils.py
@@ -8,16 +8,22 @@ def title_fmt(title):
 
 def output_fmt(text):
     padded = text.replace('\n', f'\n{H_PAD}')
-    return padded.replace("<|endoftext|>", f"{'=' * 80}{V_PAD}").replace("<|title|>", "")
+    return padded\
+        .replace("<|endoftext|>", f"{'=' * 80}{V_PAD}")\
+        .replace("<|title|>", "") + V_PAD
+
+
+def _v_spacer(text):
+    return V_PAD + "=" * 40 + f' {text} ' + "=" * 40 + V_PAD
 
 
 def get_output(text, title=None, sample=None):
-    start = V_PAD + "=" * 40 + " SAMPLE " + str(sample) + " " + "=" * 40 + V_PAD if sample is not None else ''
+    start = _v_spacer(f"SAMPLE {str(sample)}") if sample is not None else ''
 
     if title:
         start += f'{H_PAD}{title}\n\n\n\n'
 
-    return f'{V_PAD}{"=" * 80}{V_PAD}{start}' + output_fmt(text)
+    return f'{V_PAD}{start}{output_fmt(text)}{V_PAD}{_v_spacer("END")}{V_PAD}'
 
 
 def print_output(text, title=None, sample=None):
